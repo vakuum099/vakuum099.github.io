c7cfc808431ad9b38a8c18d1d79fa7cae93616ef
vakuum099 <vakuum@ukr.net>
2020-11-30 20:32:12 +0200

    2020113002 status

--- autoinsurance/pages/carousel.js
+++ autoinsurance/pages/carousel.js
@@ -2,20 +2,26 @@
 
 let images = [
     ["../images/streetview1.jpg", "Салон шин Авто-Стиль з протилежного боку проспекту імені Героїв Сталінграду."], 
-    ["../images/streetview2.jpg", "Салон шин Авто-Стиль з протилежного боку проспекту імені Героїв Сталінграду."],
-    ["../images/streetview4.jpg", "Салон шин Авто-Стиль з боку проспекту імені Героїв Сталінграду."],
+    ["../images/streetview1edit.jpg"],
+    ["../images/streetview2.jpg", "Салон шин Авто-Стиль з протилежного боку проспекту імені Героїв Сталінграду."], 
+    ["../images/streetview2edit.jpg"],
+    ["../images/streetview4.jpg", "Салон шин Авто-Стиль з боку проспекту імені Героїв Сталінграду"],
+    ["../images/streetview4edit.jpg"],
     ["../images/streetview5.jpg", "Салон шин Авто-Стиль з тротуару"],
+    ["../images/streetview5edit.jpg"],
     ["../images/streetview8.jpg", "Офіс відділення МСК на Нових домах"],
-    ["../images/streetview9.jpg", "Офіс відділення МСК на Нових домах впритул"]
+    ["../images/streetview8edit.jpg"],
+    ["../images/streetview9.jpg", "Офіс відділення МСК на Нових домах впритул"],
+    ["../images/streetview9edit.jpg"]
 ];
 
 let buttonToMap = {
     "button0": 0,
-    "button1": 1,
-    "button2": 2,
-    "button3": 3,
-    "button4": 4,
-    "button5": 5
+    "button1": 2,
+    "button2": 4,
+    "button3": 6,
+    "button4": 8,
+    "button5": 10
 };
 
 let currentPicture = 0;
@@ -27,13 +33,14 @@ window.addEventListener('load', () => {
     picture = document.querySelector("img");
     pictureButtons = document.querySelectorAll(".howtofind > button");
     pictureButtons.forEach(el => el.addEventListener('click', handleClick, false));
-    setInterval(nextPicture, 3000);
+    changePicture(0);
+    setInterval(nextPicture, 4000);
 
 }, false);
 
 function nextPicture() {
-    if (currentPicture < images.length - 1) {
-        currentPicture++;
+    if (currentPicture < images.length - 2) {
+        currentPicture += 2;
     } else {
         currentPicture = 0;
     }
@@ -44,7 +51,8 @@ function changePicture (i) {
     picture.src = images[i][0];
     picture.alt = images[i][1];
     pictureButtons.forEach(el => el.className = null);
-    pictureButtons[i].className = "active";
+    pictureButtons[i/2].className = "active";
+    setTimeout( () =>picture.src = images[i+1][0], 1500)
 }
 
 function handleClick(e) {
