c86ec576a9b7b6d99fd7659b5f3b770137702c03
vakuum099 <vakuum@ukr.net>
2020-12-01 10:54:05 +0200

    2020120101 update

--- autoinsurance/pages/carousel.js
+++ autoinsurance/pages/carousel.js
@@ -2,17 +2,17 @@
 
 let images = [
     ["../images/streetview1.jpg", "Салон шин Авто-Стиль з протилежного боку проспекту імені Героїв Сталінграду."], 
-    ["../images/streetview1edit.jpg"],
+    ["../images/streetview1edit_.jpg"],
     ["../images/streetview2.jpg", "Салон шин Авто-Стиль з протилежного боку проспекту імені Героїв Сталінграду."], 
-    ["../images/streetview2edit.jpg"],
+    ["../images/streetview2edit_.jpg"],
     ["../images/streetview4.jpg", "Салон шин Авто-Стиль з боку проспекту імені Героїв Сталінграду"],
-    ["../images/streetview4edit.jpg"],
+    ["../images/streetview4edit_.jpg"],
     ["../images/streetview5.jpg", "Салон шин Авто-Стиль з тротуару"],
-    ["../images/streetview5edit.jpg"],
+    ["../images/streetview5edit_.jpg"],
     ["../images/streetview8.jpg", "Офіс відділення МСК на Нових домах"],
     ["../images/streetview8edit.jpg"],
     ["../images/streetview9.jpg", "Офіс відділення МСК на Нових домах впритул"],
-    ["../images/streetview9edit.jpg"]
+    ["../images/streetview9edit_.jpg"]
 ];
 
 let buttonToMap = {
@@ -27,6 +27,7 @@ let buttonToMap = {
 let currentPicture = 0;
 let picture;
 let pictureButtons;
+let carousel = setInterval(nextPicture, 2000);
 
 
 window.addEventListener('load', () => {
@@ -34,8 +35,7 @@ window.addEventListener('load', () => {
     pictureButtons = document.querySelectorAll(".howtofind > button");
     pictureButtons.forEach(el => el.addEventListener('click', handleClick, false));
     changePicture(0);
-    setInterval(nextPicture, 2000);
-
+    carousel;
 }, false);
 
 function nextPicture() {
@@ -51,12 +51,18 @@ function changePicture (i) {
     picture.src = images[i][0];
     picture.alt = images[i][1];
     pictureButtons.forEach(el => el.className = null);
-    pictureButtons[i/2].className = "active";
+    pictureButtons[i/2].className = "press";
     setTimeout( () =>picture.src = images[i+1][0], 500)
 }
 
 function handleClick(e) {
     let targetIndex = buttonToMap[e.target.id];
+    clearInterval(carousel);
+    setTimeout(() => {
+        carousel = setInterval(nextPicture, 2000);
+        carousel;
+        console.log("timeout");
+    }, 7000);
     changePicture(targetIndex);
     currentPicture = targetIndex;
 }
